22b96080df243cd8affbd92e8ff794683ea2815d
jonas-lauwers <Jonas@thinkpad>
2016-08-03 16:09:45 +0300

    Changed deck to a sole purpose -> deck moved all other functions to stack class Signed-off-by:jonas-lauwers <Jonas@thinkpad>

--- src/Engine/Deck.java
+++ src/Engine/Deck.java
@@ -4,175 +4,170 @@ import java.util.*;
 import Card.Card;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 //Comparable<Object>
-public class Deck implements Serializable {
+
+
+public class Deck implements Serializable, Iterable<Card> {
 
     static final long serialVersionUID = 1337;
     private ArrayList<Card> deck;
-    private boolean isSingleCardDeck;
-    private int[] numberOfCards;
     
     /**
-     * creates a simple deck with no limit of cards, where each added cards is regarded as unique.
+     * Creates an empty deck.
      */
     public Deck() {
         deck = new ArrayList<Card>();
-        this.isSingleCardDeck = false;
     }
 
     /**
-     * Creates a deck with a limit number of cards and keeps a counter for each of those cards of how much of them are added.
-     * @param numberOfCards The total number of unique cards that will be added. 
-     */
-    public Deck(int numberOfCards) {
-        this();
-        this.numberOfCards = new int[numberOfCards];
-        this.isSingleCardDeck = true;
-    }
-
-    /**
-     * Add a card to deck, does nothing for a deck with card limit if it's full.
+     * Add a card to deck.
      * @param card The card to add.
      */
     public void add(Card card) {
-        if(isSingleCardDeck) {
-            if(deck.contains(card)) {
-                numberOfCards[deck.indexOf(card)] += 1;
-            }
-            else if(deck.size() < numberOfCards.length) {
-                deck.add(card);
-                numberOfCards[deck.indexOf(card)] = 1;
-            }
-        }
-        else {
-            this.add(deck.size(),card);
-        }
+        this.add(deck.size(),card);
     }
     
     /**
-     * Add card to the deck at a certain index, does not work for a deck with card limit.
+     * Add card to the deck at a certain index.
      * @param index The index to place the card.
      * @param card The card to add.
      */
     public void add(int index, Card card) {
-        if (!isSingleCardDeck) {
-            deck.add(index, card);
-        }
+        deck.add(index, card);
     }
-
-    
-    //TODO check whether we need this since we just can check on existance and remove it ....
-//    public Card getCard(int cardNumber) {
-//        if (!isSingleCardDeck) {
-//            return deck.get(cardNumber);
-//        } else {
-//            return deck.get(0);
-//        }
-//    }
     
     /**
-     * Remove the card from the deck. Lowers counter of numbered deck by 1 if it exits.
-     * @param card The card to remove
+     * Returns the Card thats found at the index cardNumber
+     * @param cardNumber The index of the card.
+     * @return The card at the index or null if index doesn't exist.
      */
-    //TODO check if integer version of this is still needed
-    public void remove(Card card) {
-        if(!isSingleCardDeck) {
-            deck.remove(card);
+    public Card getCard(int cardNumber) {
+        if(cardNumber < deck.size()) {
+            return deck.get(cardNumber);
         }
-        else {
-            if(deck.contains(card)) {
-                if(numberOfCards[deck.indexOf(card)] > 0 ) {
-                    numberOfCards[deck.indexOf(card)]--;
-                }
-            }
-        }
+        return null;
     }
     
-//    public void remove(int cardNumber) {
-//        if (!isSingleCardDeck) {
-//            deck.remove(cardNumber);
-//        } 
-//        else {
-//            numberOfCards[cardNumber]--;
-//        }
-//    }
+    /**
+     * Remove the card from the deck.
+     * @param card The card to remove
+     * @return True if card was in deck.
+     */
+    //TODO check if this version of this is still needed.
+    public boolean remove(Card card) {
+        return deck.remove(card);
+    }
+    
+    /**
+     * Remove the card at index cardNumber.
+     * @param cardNumber The index of the card.
+     * @return True if index existed
+     */
+    //TODO check if this version of this is still needed.
+    public boolean remove(int cardNumber) {
+        try {
+            deck.remove(cardNumber);
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
 
     /**
-     * Remove the first card of the deck and returns it. Works only on not limited card decks.
+     * Remove the first card of the deck and returns it.
      * Return Returns the first card.
      */
     public Card pop() {
-        if(!isSingleCardDeck && !deck.isEmpty()) {
+        if(!deck.isEmpty()) {
             return deck.remove(0);
         }
         return null;
     }
-//    public Card pop() {
-//        if (!isSingleCardDeck) {
-//            Card firstCard = null;
-//            for (Card c : deck) {
-//                if (c != null && firstCard == null) {
-//                    firstCard = c;
-//                    deck.remove(c);
-//                    return firstCard;
-//                }
-//            }
-//            return firstCard;
-//        }
-//        return null;
-//    }
 
     /**
-     * Shuffles the deck. Should not be used on limited card decks since the card count will not be alligned
+     * Shuffles the deck.
      */
     public void shuffle() {
         Collections.shuffle(deck);
     }
 
     /**
-     * Move a deck to another. Only for normal decks.
+     * Move the deck completely to another deck and empty it.
      * @param deck The deck where you want to move the original deck to.
+     * @return False if the given deck does not exists.
      */
-    public void moveDeckTo(Deck deck) {
-        if(!isSingleCardDeck) {
+    public boolean moveDeckTo(Deck deck) {
+        if(deck!=null) {
             deck.deck.addAll(this.deck);
-            deck.isSingleCardDeck = this.isSingleCardDeck;
-            //deck.numberOfCards = this.numberOfCards;
             this.deck.clear();
+            return true;
         }
+        return false;
     }
 
     /**
-     * Move a card from one deck to another. Only for normal decks.
+     * Move a card from one deck to another.
      * @param card The card to move.
      * @param deck The deck where to move the card to.
+     * @return True i card exists in deck.
      */
-    // duplicate moveCardToDeck ... check if the integer version is needed
-    public void moveCardToDeck(Card card, Deck deck) {
-        if(!isSingleCardDeck) {
+    //TODO Check if this version is needed.
+    public boolean moveCardToDeck(Card card, Deck deck) {
+        if(this.deck.contains(card)) {
             this.remove(card);
             deck.add(card);
+            return true;
         }
+        return false;
     }
 
-//    public void moveCardToDeck(int cardNumber, Deck deck) {
-//        Card card = this.getCard(cardNumber);
-//        this.remove(cardNumber);
-//        deck.add(card);
+    /**
+     * Move a card from one deck to another.
+     * @param cardNumber The index of the card to move.
+     * @param deck The deck where to move the card to.
+     * @return True if card exists in deck.
+     */
+    //TODO Check if this version is needed.
+    public boolean moveCardToDeck(int cardNumber, Deck deck) {
+        if(cardNumber < this.deck.size()) {
+            deck.add(this.getCard(cardNumber));
+            this.remove(cardNumber);
+            return true;
+        }
+        return false;
+    }
+    
+//    /**
+//     * Checks if the deck contains cards of the type.
+//     * @param cardType The card type you want to look for
+//     * @return True if it does contain cards.
+//     */
+//    private boolean hasCardOfType(String cardType) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+//        for (Card c : deck) {
+//            Method method = this.getClass().getDeclaredMethod("is"+cardType.replace(cardType.charAt(0), (char)(cardType.charAt(0)+26)));
+//            method.invoke(c);
+//            if (c.getType().equals(cardType)) {
+//                return true;
+//            }
+//        }
+//        return false;
 //    }
 
+    //TODO make above function functional and clean up below!!!
     /**
      * Check if the deck contains any kingdom cards.
-     * @return Returns true if it contains kingdomcards.
+     * @return Returns true if it contains kingdom cards.
      */
     public boolean hasKingdomCards() {
-        boolean hasKingdomCards = false;
         for (Card c : deck) {
             if (c.getType().equals("Action") || c.getType().equals("Attack") || c.getType().equals("Reaction")) {
-                hasKingdomCards = true;
+                return true;
             }
         }
-        return hasKingdomCards;
+        return false;
     }
     
     /**
@@ -180,60 +175,46 @@ public class Deck implements Serializable {
      * @return True if it does.
      */
     public boolean hasPlayableCards() {
-        boolean hasPlayableCards = false;
         for(Card c: deck) {
             if(!c.getPlayableTurn().equals("never")) {
-                hasPlayableCards = true;
-            }
-        }
-        return hasPlayableCards;
-    }
-
-//	public boolean hasReactionCards(){
-//		for(Card c : deck){
-//			if(c.getType().equals("Reaction")){
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-//	
-//	public boolean hasVictoryCards(){
-//		for(Card c : deck){
-//			if(c.getType().equals("Victory")){
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-//	
-//	public boolean hasTreasureCards(){
-//		for(Card c : deck){
-//			if(c.getType().equals("Treasure")){
-//				return true;
-//			}
-//		}
-//		return false;
-//	}
-    
-    /**
-     * Checks if the deck contains cards of the type.
-     * @param cardType The card type you want to look for
-     * @return True if it does contain cards.
-     */
-    public boolean hasCardOfType(String cardType) {
-        for (Card c : deck) {
-            if (c.getType().equals(cardType)) {
                 return true;
             }
         }
         return false;
     }
 
+    public boolean hasReactionCards(){
+            for(Card c : deck){
+                    if(c.getType().equals("Reaction")){
+                            return true;
+                    }
+            }
+            return false;
+    }
+
+    public boolean hasVictoryCards(){
+            for(Card c : deck){
+                    if(c.getType().equals("Victory")){
+                            return true;
+                    }
+            }
+            return false;
+    }
+
+    public boolean hasTreasureCards(){
+            for(Card c : deck){
+                    if(c.getType().equals("Treasure")){
+                            return true;
+                    }
+            }
+            return false;
+    }
+
     /**
-     * Counts all the victorypoints  that are in the given deck. Takes in acount the Garden card.
+     * Counts all the victory points  that are in the given deck. Takes in acount the Garden card.
      * @return The total amount of points in the deck.
      */
+    //TODO clean up and delegate some stuff to cards!
     public int countVictoryPoints() {
         int victoryPoints = 0;
         int gardenCard = 0;
@@ -257,46 +238,15 @@ public class Deck implements Serializable {
      * @return True if deck is empty.
      */
     public boolean isEmpty() {
-        if (!isSingleCardDeck) {
-            return deck.isEmpty();
-        }
-        throw new IllegalStateException();
-    }
-
-    /**
-     * Checks whether a limited card deck has no cards left of the given card.
-     * @param card The card to check on.
-     * @return True if cards 0 number of cards.
-     */
-    public int isEmpty(Card card) {
-        if(isSingleCardDeck) {
-            return numberOfCards[deck.indexOf(card)];
-        }
-        throw new IllegalStateException();
+        return deck.isEmpty();
     }
     
     /**
-     * Returns the size of a normal deck. For limited card deck use size(Card card).
-     * @return The size of a normal deck, or if the deck is limited amount it will return -1.
+     * Returns the size of a normal deck.
+     * @return The size of a normal deck.
      */
     public int size() {
-        if (!isSingleCardDeck) {
-            return deck.size();
-        } else {
-            return -1;
-        }
-    }
-    
-    /**
-     * Returns the size of the deck or the size of a limited deck's card.
-     * @param card The card of wich you want to return the number.
-     * @return The total number of that card in the deck.
-     */
-    public int size(Card card) {
-        if(!isSingleCardDeck)
-            return this.size();
-        else
-            return numberOfCards[deck.indexOf(card)];
+        return deck.size();
     }
 
     //TODO mae the cards comparable so we can sort them in the limited card decks( for kingdomCards deck for example)
@@ -326,24 +276,12 @@ public class Deck implements Serializable {
             return "\n";
         } else {
             String deckContent = "";
-            if (!isSingleCardDeck) {
-                int i = 1;
-                for (Card c : deck) {
-                    deckContent += String.format("\t %02d) %s", i, c.toString());
-                    i++;
-                }
-            } else {
-                deckContent = String.format("(%02d) %s", this.numberOfCards, deck.get(0).toString());
+            int i = 1;
+            for (Card c : deck) {
+                deckContent += String.format("\t %02d) %s", i, c.toString());
+                i++;
             }
             return deckContent;
-        }
-    }
-
-    //TODO This is only here for testing purpose
-    public void setCount(Card card, int count) {
-        if(isSingleCardDeck) {
-            
-            this.numberOfCards[deck.indexOf(card)] = count;
         }
     }
 
@@ -364,6 +302,11 @@ public class Deck implements Serializable {
 //        } else {
 //            return new String[]{this.getCard(0).getName(), String.valueOf(this.numberOfCards)};
 //        }
+    }
+    
+    @Override
+    public Iterator<Card> iterator() {
+        return deck.iterator();
     }
 
 }
