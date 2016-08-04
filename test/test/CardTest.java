aced5ecbea9532be56efc63c3d141caca70d8ee4
jonas-lauwers <Jonas@thinkpad>
2016-08-04 13:17:56 +0300

    Small update, all cards are in lowercase. Signed-off-by:jonas-lauwers <Jonas@thinkpad>

--- test/test/CardTest.java
+++ test/test/CardTest.java
@@ -11,13 +11,13 @@ public class CardTest {
 	@Test
 	public void convertToStringTest(){
 		Card c = new Card("Gold");
-		assertEquals("Gold       Cost: 6\n", c.toString());
+		assertEquals("gold       Cost: 6\n", c.toString());
 		
 		c = new Card("Village");
-		assertEquals("Village              Cost: 3 - Actions: 2 - Buys: 0 - Coins: 0 - Cards: 1\n", c.toString());
+		assertEquals("village              Cost: 3 - Actions: 2 - Buys: 0 - Coins: 0 - Cards: 1\n", c.toString());
 		
 		c = new Card("Cellar");
-		assertEquals("Cellar               Cost: 2 - Actions: 1 - Buys: 0 - Coins: 0 - Cards: 0 - Description: Discard any number of cards. +1 Card per card discarded.\n", c.toString());
+		assertEquals("cellar               Cost: 2 - Actions: 1 - Buys: 0 - Coins: 0 - Cards: 0 - Description: Discard any number of cards. +1 Card per card discarded.\n", c.toString());
 	}
 	
 
