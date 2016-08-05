a665269183c759511b51c7ba4cb86b35efd9cee7
jonas-lauwers <Jonas@thinkpad>
2016-08-05 12:11:31 +0300

    Update on toString() Signed-off-by:jonas-lauwers <Jonas@thinkpad>

--- src/Engine/Deck.java
+++ src/Engine/Deck.java
@@ -6,12 +6,11 @@ import Card.Card;
 import java.io.Serializable;
 //Comparable<Object>
 
-
 public class Deck implements Serializable, Iterable<Card> {
 
     static final long serialVersionUID = 1337;
     private ArrayList<Card> deck;
-    
+
     /**
      * Creates an empty deck.
      */
@@ -21,35 +20,39 @@ public class Deck implements Serializable, Iterable<Card> {
 
     /**
      * Add a card to deck.
+     *
      * @param card The card to add.
      */
     public void add(Card card) {
-        this.add(deck.size(),card);
+        this.add(deck.size(), card);
     }
-    
+
     /**
      * Add card to the deck at a certain index.
+     *
      * @param index The index to place the card.
      * @param card The card to add.
      */
     public void add(int index, Card card) {
         deck.add(index, card);
     }
-    
+
     /**
      * Returns the Card thats found at the index cardNumber
+     *
      * @param cardNumber The index of the card.
      * @return The card at the index or null if index doesn't exist.
      */
     public Card getCard(int cardNumber) {
-        if(cardNumber < deck.size()) {
+        if (cardNumber < deck.size()) {
             return deck.get(cardNumber);
         }
         return null;
     }
-    
+
     /**
      * Remove the card from the deck.
+     *
      * @param card The card to remove
      * @return True if card was in deck.
      */
@@ -57,9 +60,10 @@ public class Deck implements Serializable, Iterable<Card> {
     public boolean remove(Card card) {
         return deck.remove(card);
     }
-    
+
     /**
      * Remove the card at index cardNumber.
+     *
      * @param cardNumber The index of the card.
      * @return True if index existed
      */
@@ -68,18 +72,19 @@ public class Deck implements Serializable, Iterable<Card> {
         try {
             deck.remove(cardNumber);
             return true;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
     }
 
     /**
      * Remove the first card of the deck and returns it.
-     * Return Returns the first card.
+     *
+     * @return Returns the first card.
+     *
      */
     public Card pop() {
-        if(!deck.isEmpty()) {
+        if (!deck.isEmpty()) {
             return deck.remove(0);
         }
         return null;
@@ -94,11 +99,12 @@ public class Deck implements Serializable, Iterable<Card> {
 
     /**
      * Move the deck completely to another deck and empty it.
+     *
      * @param deck The deck where you want to move the original deck to.
      * @return False if the given deck does not exists.
      */
     public boolean moveDeckTo(Deck deck) {
-        if(deck!=null) {
+        if (deck != null) {
             deck.deck.addAll(this.deck);
             this.deck.clear();
             return true;
@@ -108,13 +114,14 @@ public class Deck implements Serializable, Iterable<Card> {
 
     /**
      * Move a card from one deck to another.
+     *
      * @param card The card to move.
      * @param deck The deck where to move the card to.
      * @return True i card exists in deck.
      */
     //TODO: Check if this version is needed.
     public boolean moveCardToDeck(Card card, Deck deck) {
-        if(this.deck.contains(card)) {
+        if (this.deck.contains(card)) {
             this.remove(card);
             deck.add(card);
             return true;
@@ -124,20 +131,21 @@ public class Deck implements Serializable, Iterable<Card> {
 
     /**
      * Move a card from one deck to another.
+     *
      * @param cardNumber The index of the card to move.
      * @param deck The deck where to move the card to.
      * @return True if card exists in deck.
      */
     //TODO: Check if this version is needed.
     public boolean moveCardToDeck(int cardNumber, Deck deck) {
-        if(cardNumber < this.deck.size()) {
+        if (cardNumber < this.deck.size()) {
             deck.add(this.getCard(cardNumber));
             this.remove(cardNumber);
             return true;
         }
         return false;
     }
-    
+
 //    /**
 //     * Checks if the deck contains cards of the type.
 //     * @param cardType The card type you want to look for
@@ -153,10 +161,10 @@ public class Deck implements Serializable, Iterable<Card> {
 //        }
 //        return false;
 //    }
-
     //TODO: make above function functional and clean up below!!!
     /**
      * Check if the deck contains any kingdom cards.
+     *
      * @return Returns true if it contains kingdom cards.
      */
     public boolean hasKingdomCards() {
@@ -167,49 +175,52 @@ public class Deck implements Serializable, Iterable<Card> {
         }
         return false;
     }
-    
+
     /**
      * Check if the deck contains cards that can be played.
+     *
      * @return True if it does.
      */
     public boolean hasPlayableCards() {
-        for(Card c: deck) {
-            if(c.isPlayable()) {
+        for (Card c : deck) {
+            if (c.isPlayable()) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean hasReactionCards(){
-            for(Card c : deck){
-                    if(c.getType().equals("Reaction")){
-                            return true;
-                    }
+    public boolean hasReactionCards() {
+        for (Card c : deck) {
+            if (c.isReaction()) {
+                return true;
             }
-            return false;
+        }
+        return false;
     }
 
-    public boolean hasVictoryCards(){
-            for(Card c : deck){
-                    if(c.getType().equals("Victory")){
-                            return true;
-                    }
+    public boolean hasVictoryCards() {
+        for (Card c : deck) {
+            if (c.isVictory()) {
+                return true;
             }
-            return false;
+        }
+        return false;
     }
 
-    public boolean hasTreasureCards(){
-            for(Card c : deck){
-                    if(c.getType().equals("Treasure")){
-                            return true;
-                    }
+    public boolean hasTreasureCards() {
+        for (Card c : deck) {
+            if (c.isTreasure()) {
+                return true;
             }
-            return false;
+        }
+        return false;
     }
 
     /**
-     * Counts all the victory points  that are in the given deck. Takes in acount the Garden card.
+     * Counts all the victory points that are in the given deck. Takes in acount
+     * the Garden card.
+     *
      * @return The total amount of points in the deck.
      */
     //TODO: clean up and delegate some stuff to cards!
@@ -232,15 +243,18 @@ public class Deck implements Serializable, Iterable<Card> {
     }
 
     /**
-     * Checks whether a normal deck is empty. For limited card decks us isEmpty(Card card)
+     * Checks whether a normal deck is empty. For limited card decks us
+     * isEmpty(Card card)
+     *
      * @return True if deck is empty.
      */
     public boolean isEmpty() {
         return deck.isEmpty();
     }
-    
+
     /**
      * Returns the size of a normal deck.
+     *
      * @return The size of a normal deck.
      */
     public int size() {
@@ -267,7 +281,6 @@ public class Deck implements Serializable, Iterable<Card> {
 //            throw new IllegalArgumentException();
 //        }
 //    }
-
     @Override
     public String toString() {
         if (deck.isEmpty()) {
@@ -276,7 +289,7 @@ public class Deck implements Serializable, Iterable<Card> {
             String deckContent = "";
             int i = 1;
             for (Card c : deck) {
-                deckContent += String.format("\t %02d) %s", i, c.toString());
+                deckContent += String.format("\t%02d) %s", i, c.toString());
                 i++;
             }
             return deckContent;
@@ -289,19 +302,19 @@ public class Deck implements Serializable, Iterable<Card> {
             return new String[]{};
         }
 //        if (!isSingleCardDeck) {
-            String[] deckArray = new String[deck.size()];
-            for(Card c : deck) {
-                deckArray[deckArray.length] = c.getName();
-            }
+        String[] deckArray = new String[deck.size()];
+        for (Card c : deck) {
+            deckArray[deckArray.length] = c.getName();
+        }
 //            for (int i = 0; i < deck.size(); i++) {
 //                deckArray[i] = this.getCard(i).getName();
 //            }
-            return deckArray;
+        return deckArray;
 //        } else {
 //            return new String[]{this.getCard(0).getName(), String.valueOf(this.numberOfCards)};
 //        }
     }
-    
+
     @Override
     public Iterator<Card> iterator() {
         return deck.iterator();
