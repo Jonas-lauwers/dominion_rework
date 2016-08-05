8444137beae937fb91d21ca12a72eee682f6e070
jonas-lauwers <Jonas@thinkpad>
2016-08-05 15:41:21 +0300

    Added doc to Player and did cleanup

--- src/Engine/Player.java
+++ src/Engine/Player.java
@@ -4,93 +4,151 @@ import java.util.*;
 
 import java.io.Serializable;
 
-public class Player implements Serializable{
-	
-	static final long serialVersionUID = 1337;
-	private String name;
-	private int actions;
-	private int buys;
-	private int coins;
-	private int score;
-	private Map<String , Deck> PlayingField;
-	
-	public Player(String name) {
-		this.name = name;
-		this.actions = 1;
-		this.buys = 1;
-		this.coins = 0;
-		this.score = 0;
-		this.PlayingField = new HashMap<>();
-		this.PlayingField.put("trash", new Deck());
-		this.PlayingField.put("discard", new Deck());
-		this.PlayingField.put("table", new Deck());
-		this.PlayingField.put("deck", new Deck());
-		this.PlayingField.put("hand", new Deck());
-	}
-	
-	
-	public Deck getDeck(String deckName){
-		return this.PlayingField.get(deckName.toLowerCase());
-	}
-	
-        //TODO: do we need to have this?
-	public Map<String , Deck> getAllDecks(){
-		return this.PlayingField;
-	}
-	
-	public int getCoins(){
-		return this.coins;
-	}
-	
-	public void addCoins(int coins){
-		this.coins += coins;
-	}
-	
-	public int getActions(){
-		return this.actions;
-	}
-	
-	public void addActions(int actions) {
-		this.actions += actions;
-	}
-	
-	public int getBuys() {
-		return this.buys;
-	}
-	
-	public void addBuys(int buys) {
-		this.buys += buys;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public void endPlayerTurn() {
-		this.PlayingField.get("hand").moveDeckTo(this.PlayingField.get("discard"));
-		this.PlayingField.get("table").moveDeckTo(this.PlayingField.get("discard"));
-		this.buys = 1;
-		this.actions = 1;
-		this.coins = 0;
-	}
-	
-	public void setScore(int score) {
-		this.score = score;
-	}
-	
-	public int getScore() {
-		return this.score;
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if(o instanceof Player) {
-			Player p = (Player) o;
-			if (this.name.equals(p.name)) {
-				return true;
-			}
-			else return false;
-		}
-		else throw new IllegalArgumentException();
-	}
+/**
+ * A Container class for players in dominion. Holds all the necessary information
+ * for a player.
+ * @author Jonas Lauwers <jonas.lauwers AT gmail.org>
+ */
+public class Player implements Serializable {
+
+    static final long serialVersionUID = 1337;
+    private final String name;
+    private int actions;
+    private int buys;
+    private int coins;
+    private int score;
+    private final Map<String, Deck> playingField;
+
+    /**
+     * Initialise a player object.
+     * @param name The name of the player.
+     */
+    public Player(String name) {
+        this.name = name;
+        this.actions = 1;
+        this.buys = 1;
+        this.coins = 0;
+        this.score = 0;
+        this.playingField = new HashMap<>();
+        this.playingField.put("trash", new Deck());
+        this.playingField.put("discard", new Deck());
+        this.playingField.put("table", new Deck());
+        this.playingField.put("deck", new Deck());
+        this.playingField.put("hand", new Deck());
+    }
+
+    /**
+     * Get the deck of the specified deckName.
+     * @param deckName The name of the deck.
+     * @return The deck object or null if the deckName does not exist.
+     */
+    public Deck getDeck(String deckName) {
+        return this.playingField.get(deckName);
+    }
+    
+    /**
+     * Get the value of the players coins.
+     * @return Value of coins.
+     */
+    public int getCoins() {
+        return this.coins;
+    }
+
+    /**
+     * Add the value coins to the coins of the player.
+     * @param coins The value of coins to add.
+     */
+    public void addCoins(int coins) {
+        this.coins += coins;
+    }
+
+    /**
+     * Get the value of the players actions.
+     * @return Value of actions.
+     */
+    public int getActions() {
+        return this.actions;
+    }
+
+    /**
+     * Add the value actions to the actions of the player.
+     * @param actions The value of actions to add.
+     */
+    public void addActions(int actions) {
+        this.actions += actions;
+    }
+    
+    /**
+     * Get the value of the players buys.
+     * @return Value of buys.
+     */
+    public int getBuys() {
+        return this.buys;
+    }
+
+    /**
+     * Add the value buys to the buys of the player.
+     * @param buys The value of buys to add.
+     */
+    public void addBuys(int buys) {
+        this.buys += buys;
+    }
+
+    /**
+     * Get the value of the players name.
+     * @return Value of the name.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Get the value of the players score.
+     * @return Value of score.
+     */
+    public int getScore() {
+        return this.score;
+    }
+
+    /**
+     * Set the value score of the score of the player.
+     * @param score The value of score.
+     */
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * Reset player to it's turn start state.
+     */
+    //TODO move to engine probably .... player doesn't know what it's initial state is, engine does.
+    public void endPlayerTurn() {
+        this.playingField.get("hand").moveDeckTo(this.playingField.get("discard"));
+        this.playingField.get("table").moveDeckTo(this.playingField.get("discard"));
+        this.buys = 1;
+        this.actions = 1;
+        this.coins = 0;
+    }
+
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Player) {
+            Player p = (Player) o;
+            if (this.name.equals(p.name)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
 }
